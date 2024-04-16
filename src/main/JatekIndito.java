0019cc1170c5137c84360c8fce050cac01809d41
patrikharman <patrikemailcim@gmail.com>
2024-04-16 13:58:33 +0200

    jatek

---
+++ src/main/JatekIndito.java
@@ -0,0 +1,19 @@
+
+package main;
+
+
+public class JatekIndito {
+
+    
+    public static void main(String[] args) {
+        /*referencia nélkül példányosítjuk*/
+        //new Jatek().start();
+        //Jatek referencia = new Jatek();
+        //referencia.start();
+        /*rfg nélkül csak a konstruktort hívjuk*/
+        new Jatek();
+        
+        new Jatekos().tippel();
+    }
+    
+}
